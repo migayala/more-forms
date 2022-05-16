0735a687f3097ac32efd9e64fcaad7efde10d3bc
migayala <mayalamontoya@gmail.com>
2022-05-15 22:48:16 -0700

    modified code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from  'react';
+    
+const App = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPw, setConfirmPw] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <form onSubmit={() => {}}>
+          <div>
+            <label htmlFor="">First Name</label>
+            <input type="text" onChange={(e) => setFirstName(e.target.value)} />
+          </div>
+          {firstName.length < 2 ? (<p>First Name must be at least 2 characters</p> ) : null}
+          <div>
+            <label htmlFor="">Last Name</label>
+            <input type="text" onChange={(e) => setLastName(e.target.value)} />
+          </div>
+          {lastName.length < 2 ? (<p>Last Name must be at least 2 characters</p> ) : null}
+          <div>
+            <label htmlFor="">Email</label>
+            <input type="text" onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          {email.length < 5 ? <p>Email must be at least 5 characters</p> : null}
+          <div>
+            <label htmlFor="">Password</label>
+            <input type="text" onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          {password.length < 8 ? (
+            <p>Password must be at least 8 characters</p>
+          ) : null}
+          <div>
+            <label htmlFor="">Confirm Password</label>
+            <input type="text" onChange={(e) => setConfirmPw(e.target.value)} />
+          </div>
+          {confirmPw !== password ? <p>Passwords must match</p> : null}
+        </form>
+      </>
     </div>
   );
-}
+};
 
 export default App;
